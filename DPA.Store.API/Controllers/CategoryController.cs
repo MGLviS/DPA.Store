3d1e79c23cd4c4d7f7270606b0caea736cb8c280
MGLviS <22101571@ue.edu.pe>
2023-09-23 21:32:50 -0500

    Prototipo 02

--- DPA.Store.API/Controllers/CategoryController.cs
+++ DPA.Store.API/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using DPA.Store.DOMAIN.Core.Interfaces;
+﻿using DPA.Store.DOMAIN.Core.Entities;
+using DPA.Store.DOMAIN.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,11 +16,31 @@ namespace DPA.Store.API.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        [HttpGet]
+        [HttpGet("GetAll")]
         public async Task<IActionResult> GetAll()
         {
             var categories = await _categoryRepository.GetAll();
             return Ok(categories);
         }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var categories = await _categoryRepository.GetById(id);
+            return Ok(categories);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var result = await _categoryRepository.Insert(category);
+            if (!result)
+                return BadRequest(result);
+            return Ok(result);
+        }
+
+        //[HttpDelete("Delete")]
+
+ 
     }
 }
