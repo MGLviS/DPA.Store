9691f896616366e80cb8309f8c7dfb870ed64703
MGLviS <22101571@ue.edu.pe>
2023-09-23 20:39:42 -0500

    Category Controller

--- DPA.Store.API/Program.cs
+++ DPA.Store.API/Program.cs
@@ -1,4 +1,6 @@
+using DPA.Store.DOMAIN.Core.Interfaces;
 using DPA.Store.DOMAIN.Infrastructure.Data;
+using DPA.Store.DOMAIN.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,10 @@ builder.Services.AddDbContext<StoreDbContext>(options =>
     options.UseSqlServer(_cnx);
 });
 
+
+builder
+    .Services
+    .AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ DPA.Store.DOMAIN/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using DPA.Store.DOMAIN.Core.Entities;
+using DPA.Store.DOMAIN.Core.Interfaces;
+using DPA.Store.DOMAIN.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA.Store.DOMAIN.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public CategoryRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Ienumerable devuelve un listado, puede cambiar el tipo variable
+        //async enviar peticiones de forma asincrona// return await
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _dbContext.Category.ToListAsync(); //Retorna una lista, en este las categorías
+        }
+    }
+}
